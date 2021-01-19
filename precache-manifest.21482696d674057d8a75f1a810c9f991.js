865d72c66922b6b8ae1858de048ebdeff7679460
RioDron <riopositivniy@gmail.com>
2021-01-19 18:22:01 +0200

    Updates

---
+++ precache-manifest.21482696d674057d8a75f1a810c9f991.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39e5df67d36233a48e9cfdcf26a0555a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c83f55a9ed0f78ee5e41",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "7c7a19eaf806aa4e071d",
+    "url": "./static/js/2.cad20d42.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cad20d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c83f55a9ed0f78ee5e41",
+    "url": "./static/js/main.02423b58.chunk.js"
+  },
+  {
+    "revision": "2a286f7172708a7e8c5a",
+    "url": "./static/js/runtime-main.3a2c9fb2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.21482696d674057d8a75f1a810c9f991.js"
 );
 
 self.addEventListener('message', (event) => {
